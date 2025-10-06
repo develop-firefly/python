3fdb46012b3486930e9156dc3f303fe00e9bfdb9
develop-firefly <mr_develop@outlook.com>
2025-10-05 23:39:17 -0500

    python_cxoracle

--- python-cxoracle/useful-functions/db_commit.py
+++ python-cxoracle/useful-functions/db_commit.py
@@ -1,5 +1,7 @@
-# Method to manually commit the sql
-# Argument to this method: None
-# Note: This is defined, since connection.auto commit is set to False by default, and I have kept it that way
 def db_commit(self):
+    '''
+    Method to manually commit the sql
+    Argument to this method: None
+    Note: This is defined, since connection.auto commit is set to False by default, and I have kept it that way
+    '''
     self.db_auto_connect.commit()
