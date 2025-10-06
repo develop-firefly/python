ad625210f26917ff6b1c4972196cc9aec416b93e
develop-firefly <mr_develop@outlook.com>
2025-10-06 07:49:17 -0500

    python_cxoracle

--- python-cxoracle/useful-functions/db_execute_sql_as_sysdba.py
+++ python-cxoracle/useful-functions/db_execute_sql_as_sysdba.py
@@ -1,6 +1,9 @@
-# Method to execute any sql as sysdba
-# Argument to this method is: SQL Query or Variable containing the SQL query
 def db_execute_sql_as_sysdba(self, _sql_query_or_sql_variable):
+    '''
+    Method to execute any sql as sysdba
+    Argument to this method is: SQL Query or Variable containing the SQL query
+    Return type of this method is a tuple (default behavior)
+    '''
     # Open the cursor as 'with' so, it's automatically closed upon task completion
     with self.db_sys_privileged_conn().cursor() as sysdba_cursor:
         # Execute the SQL Query or Variable containing the SQL query as SYSDBA
