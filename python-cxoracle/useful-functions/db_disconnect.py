8e9378574e11e930fa25fbe0e25f6493246b0c11
develop-firefly <mr_develop@outlook.com>
2025-10-06 07:48:07 -0500

    python_cxoracle

--- python-cxoracle/useful-functions/db_disconnect.py
+++ python-cxoracle/useful-functions/db_disconnect.py
@@ -1,5 +1,7 @@
-# Method to close the connection to database
-# Argument to this method: None
-# Note: This method is manual, since I did not use auto object / connection termination using 'with'
 def db_disconnect(self):
+    '''
+    Method to close the connection to database
+    Argument to this method: None
+    Note: This method is manual, since I did not use auto object / connection termination using 'with'
+    '''
     self.db_auto_connect.close()
