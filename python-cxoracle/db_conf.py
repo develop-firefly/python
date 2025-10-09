10f263603f1e228d711723a1e2da6d76cfecda23
develop-firefly <mr_develop@outlook.com>
2025-10-09 07:27:30 -0400

    python_cxoracle

--- python-cxoracle/db_conf.py
+++ python-cxoracle/db_conf.py
@@ -1,5 +1,5 @@
-# Import cx_oracle package: This is Python API for Oracle DB connection and Transaction
-import cx_oracle
+# Import cx_Oracle package: This is Python API for Oracle DB connection and Transaction
+import cx_Oracle
 
 # Path to oracle client on local machine
 ora_client_config = {'orcl_client_path': '/usr/lib/oracle/21/client64'}
@@ -21,12 +21,12 @@ soda_user_config = {'user': 'sodauser',
 
 # Comment Specific only for privileged connection
 '''
-Do not convert value of mode to a string as 'cx_oracle.SYSDBA', since cx_oracle.SYSDBA eventually
+Do not convert value of mode to a string as 'cx_Oracle.SYSDBA', since cx_Oracle.SYSDBA eventually
 refers to mode = 2. if its converted to string you will continue to see
 TypeError: an integer is required (got type str) while attempting to connect
 '''
 privileged_user = {'user': 'SYS',
                    'password': 'password',
                    'dsn': db_config['host'] + ':' + str(db_config['port']) + '/' + db_config['service_name'],
-                   'mode': cx_oracle.SYSDBA}
+                   'mode': cx_Oracle.SYSDBA}
 
