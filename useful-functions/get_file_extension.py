8bf1a671b7ec8516fd259820d964b0ebd79fec0b
develop-firefly <mr_develop@outlook.com>
2025-10-12 08:23:10 -0400

    useful_functions

--- useful-functions/get_file_extension.py
+++ useful-functions/get_file_extension.py
@@ -2,6 +2,7 @@ def get_file_extension(self, _filename):
     '''
     Method to get the file extension
     Input: Filename
+    Output: File extension
     '''
     # Using the split method of class <str>
     # split the filename using '.' and capturing the last element of the return type class <list>
