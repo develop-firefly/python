57e45352412a0379a5933023dd140b88c056494e
develop-firefly <mr_develop@outlook.com>
2025-10-12 08:23:17 -0400

    useful_functions

--- useful-functions/get_current_date.py
+++ useful-functions/get_current_date.py
@@ -3,6 +3,8 @@ from datetime import datetime
 def get_current_date():
     '''
     Method to get the current date
+    Input: None
+    Output: Current date in the format MM/DD/YYYY
     '''
     fmt = "%m/%d/%Y"
     return datetime.today().strftime(format=fmt)
