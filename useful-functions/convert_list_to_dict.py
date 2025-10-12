1b44bf318239e0af54c7d93d937f033020c56859
develop-firefly <mr_develop@outlook.com>
2025-10-12 08:22:34 -0400

    useful_functions

--- useful-functions/convert_list_to_dict.py
+++ useful-functions/convert_list_to_dict.py
@@ -3,6 +3,7 @@ def convert_list_to_dict(inp_list, starting_idx_position):
     Method to convert a list to a dictionary
     Input:1 - Iterable Sequence
     Input:2 - Starting Counter position
+    Output: Dictionary with counter as key and iterable members as values
     '''
     # define an empty dictionary
     output_dict = dict()
--- useful-functions/convert_set_to_dict.py
+++ useful-functions/convert_set_to_dict.py
@@ -3,6 +3,7 @@ def convert_set_to_dict(inp_set, starting_idx_position):
     Method to convert a set to a dictionary
     Input:1 - Iterable Sequence
     Input:2 - Starting Counter position
+    Output: Dictionary with counter as key and iterable members as values
     '''
     # define an empty dictionary
     output_dict = dict()
