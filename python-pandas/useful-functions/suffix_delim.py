a77be8004f6b4db8e0939281e498c525877bd623
develop-firefly <mr_develop@outlook.com>
2025-10-14 07:50:22 -0400

    python_pandas

--- python-pandas/useful-functions/suffix_delim.py
+++ python-pandas/useful-functions/suffix_delim.py
@@ -1,6 +1,7 @@
 def suffix_delim(self, _tgt_col_name, _delim):
     '''
     Method to add a delimiter as suffix
-    Input Arguments: - Target column name, Delimiter to be added as suffix
+    Input: - Target column name, Delimiter to be added as suffix
+    Output: - None
     '''
     self.my_df[_tgt_col_name] = self.my_df[_tgt_col_name].astype(str) + _delim
