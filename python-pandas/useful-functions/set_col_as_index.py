63e6acdee19d7446b624e9aa95b79d1e71e81a26
develop-firefly <mr_develop@outlook.com>
2025-10-14 07:49:40 -0400

    useful_functions

--- python-pandas/useful-functions/set_col_as_index.py
+++ python-pandas/useful-functions/set_col_as_index.py
@@ -1,6 +1,7 @@
 def set_col_as_index(self, _col_name):
     '''
     Method to set a column as the index column
-    Input Argument: - Column name which needs to be set as index
+    Input: - Column name which needs to be set as index
+    Output: - DataFrame with the specified column set as index
     '''
     return self.my_df.set_index(_col_name)
--- python-pandas/useful-functions/value_frequency_in_col_by_idx.py
+++ python-pandas/useful-functions/value_frequency_in_col_by_idx.py
@@ -1,6 +1,7 @@
 def value_frequency_in_col_by_idx(self, _col_idx):
     '''
     Method to get frequency of occurrences per unique item in a given column
-    Input Argument: - Column index position
+    Input: - Column index position
+    Output: - Series with unique items as index and their frequency as values
     '''
     return self.my_df[self.my_df_col_idx_name[_col_idx]].value_counts()
