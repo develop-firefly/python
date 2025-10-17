67010fb5ecf38815039c417b2e7fb3b80231c513
develop-firefly <mr_develop@outlook.com>
2025-10-17 07:18:52 -0500

    python_pandas

---
+++ python-pandas/useful-functions/create_df_from_filtered_data_single_not_in_condition.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def create_df_from_filtered_data_single_not_in_condition(self, _column_name, _condition):
+    '''
+    Create Dataframe from filtered data based on single condition
+    Input:1 - param _column_name: Column name where condition is to be applied
+    Input:2 - param _condition: Condition to be applied on the column
+    '''
+    # create a new filtered dataset based on the condition applied on the column
+    _filtered_data_set = (self.my_df.loc[self.my_df[_column_name] != _condition])
+    # return Dataframe
+    return pd.DataFrame(_filtered_data_set)
